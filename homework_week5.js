1073f3afdb4b0f886cc3c9647d429ba7e31e0296
Christina <98745726+ChristinaFreisleben@users.noreply.github.com>
2022-02-01 11:52:42 +0100

    Existing CSS, HTML and JS files added to repository

---
+++ homework_week5.js
@@ -0,0 +1,116 @@
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let date = now.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let currentDate = document.querySelector("#current-date");
+currentDate.innerHTML = `${day}, ${date} ${month}`;
+
+let hour = String(now.getHours()).padStart(2, "0");
+let minutes = String(now.getMinutes()).padStart(2, "0");
+let currentTime = document.querySelector("#current-time");
+
+currentTime.innerHTML = `${hour}:${minutes}`;
+
+let searchForm = document.querySelector("#search-form");
+
+function changeCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = `<strong>${input.value}</strong>`;
+
+  let apiKey = "cc9d95ff70494e22ebe5e4ea828d0369";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  function showWeatherData(response) {
+    console.log(response.data);
+    let apiCelciusTemperature = Math.round(response.data.main.temp);
+    let celciusTemperature = document.querySelector("#temperature");
+    celciusTemperature.innerHTML = apiCelciusTemperature;
+
+    let apiHumidity = Math.round(response.data.main.humidity);
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = apiHumidity;
+
+    let apiWind = Math.round(response.data.wind.speed);
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = apiWind;
+
+    let apiTempMax = Math.round(response.data.main.temp_max);
+    let tempMax = document.querySelector("#temp-max");
+    tempMax.innerHTML = apiTempMax;
+
+    let apiTempMin = Math.round(response.data.main.temp_min);
+    let tempMin = document.querySelector("#temp-min");
+    tempMin.innerHTML = apiTempMin;
+  }
+
+  axios.get(apiUrl).then(showWeatherData);
+}
+
+searchForm.addEventListener("submit", changeCity);
+
+function showCurrentCity(event) {
+  function showPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = "cc9d95ff70494e22ebe5e4ea828d0369";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    function showCurrentWeatherData(response) {
+      console.log(response.data);
+      let apiCityName = response.data.name;
+      let cityName = document.querySelector("#current-city");
+      cityName.innerHTML = `<strong>${apiCityName}</strong>`;
+
+      let apiCelciusTemperature = Math.round(response.data.main.temp);
+      let celciusTemperature = document.querySelector("#temperature");
+      celciusTemperature.innerHTML = apiCelciusTemperature;
+
+      let apiHumidity = Math.round(response.data.main.humidity);
+      let humidity = document.querySelector("#humidity");
+      humidity.innerHTML = apiHumidity;
+
+      let apiWind = Math.round(response.data.wind.speed);
+      let wind = document.querySelector("#wind");
+      wind.innerHTML = apiWind;
+
+      let apiTempMax = Math.round(response.data.main.temp_max);
+      let tempMax = document.querySelector("#temp-max");
+      tempMax.innerHTML = apiTempMax;
+
+      let apiTempMin = Math.round(response.data.main.temp_min);
+      let tempMin = document.querySelector("#temp-min");
+      tempMin.innerHTML = apiTempMin;
+    }
+    axios.get(apiUrl).then(showCurrentWeatherData);
+  }
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentCityButton = document.querySelector("#current-city-button");
+currentCityButton.addEventListener("click", showCurrentCity);
